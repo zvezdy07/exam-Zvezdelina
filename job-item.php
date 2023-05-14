481c74e55aed3c655ad4e405a0e2bdef7d3e7158
zvezdy07 <127771538+zvezdy07@users.noreply.github.com>
2023-05-14 15:51:51 +0300

    Add files via upload

---
+++ job-item.php
@@ -0,0 +1,20 @@
+<li class="job-card">
+    <div class="job-primary">
+        <h2 class="job-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </h2>
+        <div class="job-meta">
+            <a class="meta-company" href="#">Best products for you Ltd.</a>
+            <span class="meta-date"><?php the_date(); ?></span>
+        </div>
+        <div class="job-details">
+            <span class="job-location">Location</span>
+            <span class="job-type">Contract staff</span>
+        </div>
+    </div>
+    <div class="job-logo">
+        <div class="job-logo-box">
+       
+            <img src="http://localhost/exam-zvezdelina/wp-content/uploads/2023/05/washing-machine.jpg" alt="">
+        </div>
+    </div>
+</li>
