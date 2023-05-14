954f977882bac7939864bdbbaaf34f4e5c40b442
zvezdy07 <127771538+zvezdy07@users.noreply.github.com>
2023-05-14 15:50:38 +0300

    Add files via upload

---
+++ zvezdelina-jobs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Exam Zvezdelina
+ * Author: Zvezdelina Ivanova
+ * Description: My SoftUni board plugin
+ * 
+ */
+
+// var_dump( 'index'); die();
+
+require 'cpt-jobs.php' ;
+require 'functions.php';
